a9ba1150126d995082af53e88ab15173e674c8eb
Andrzej Jabcoń <andrzej.jabcon@gmail.com>
2019-02-13 10:11:53 +0100

    Football Backend Net.Core WebAPI App

---
+++ Lesson 23 - Angular - Football Web App/Backend_Net.Core/FootballWebServer/FootballWebServer/Controllers/TeamController.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FootballWebServer.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/Team")]
+    public class TeamController : Controller
+    {
+        [HttpGet]
+        public void Index()
+        {
+        }
+
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "Team A";
+        }
+
+        [HttpPost]
+        public void Post([FromBody]string value)
+        {
+        }
+
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
